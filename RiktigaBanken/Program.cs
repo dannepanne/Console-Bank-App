b21c27d1769c85238b407c057fc981fc97b88d78
Daniel Lenberg <danneeeee@hotmail.com>
2021-10-07 16:12:58 +0200

    fixz

--- RiktigaBanken/Program.cs
+++ RiktigaBanken/Program.cs
@@ -9,14 +9,16 @@ namespace RiktigaBanken
             try
             { BankLogic.ReadText(); }
             catch (Exception e)
-            { Console.WriteLine($"Kan inte öppna Kundlista.txt för att {e}"); }
-            Console.ReadKey();
+            {
+                Console.WriteLine($"Kan inte öppna Kundlista.txt för att {e}");
+                Console.ReadKey();
+            }
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.Clear();
 
             BankMenu.Menu();
-            
+
 
 
         }
