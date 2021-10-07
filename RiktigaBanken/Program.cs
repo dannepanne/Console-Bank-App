694c721aba0cd8b47aecfd82df341c9c918778fe
Daniel Lenberg <danneeeee@hotmail.com>
2021-10-07 16:11:34 +0200

    stavfel

--- RiktigaBanken/Program.cs
+++ RiktigaBanken/Program.cs
@@ -9,7 +9,7 @@ namespace RiktigaBanken
             try
             { BankLogic.ReadText(); }
             catch (Exception e)
-            { Console.WriteLine($"Kan inte öpna Kundlista.txt för att {e}"); }
+            { Console.WriteLine($"Kan inte öppna Kundlista.txt för att {e}"); }
             Console.ReadKey();
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Blue;
